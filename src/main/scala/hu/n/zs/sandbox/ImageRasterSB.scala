d0a117d2b3c55b39f062cae4169d4fb469adf232
Nemestóthy Zsombor <nemzsom@gmail.com>
2014-01-10 15:16:29 +0100

    Playing with image representationsl

--- src/main/scala/hu/n/zs/sandbox/ImageRasterSB.scala
+++ src/main/scala/hu/n/zs/sandbox/ImageRasterSB.scala
@@ -3,8 +3,9 @@ package hu.n.zs.sandbox
 import scala.swing.Swing._
 import scala.swing.{Panel, MainFrame, SimpleSwingApplication}
 import java.awt.{Graphics2D, Color}
-import java.awt.image.{DataBufferInt, SampleModel, Raster, BufferedImage}
+import java.awt.image._
 import java.util
+import sun.security.provider.certpath.IndexedCollectionCertStore
 
 object ImageRasterSB extends SimpleSwingApplication {
 
@@ -14,8 +15,9 @@ object ImageRasterSB extends SimpleSwingApplication {
   lazy val ui = new Panel {
     background = Color.white
     preferredSize = (width, height)
-    val cm = new IterationColorModel(width + height)
 
+    // test custom implementation
+    val cm = new IterationColorModel(width + height)
     val buffer = {
       val data = new Array[Int](width * height)
       for {
@@ -28,14 +30,29 @@ object ImageRasterSB extends SimpleSwingApplication {
     val raster = Raster.createPackedRaster(buffer, width, height, 32, null)
     var bufferedImage: BufferedImage = new BufferedImage(cm, raster, false, null)
 
-
-    bufferedImage = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB)
-
+    // test rgb
+    bufferedImage = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB)
+    val max = width + height
     for {
       x <- 0 until width
       y <- 0 until height
-      i = x + (y * width)
-    } bufferedImage.setRGB(x, y, i << 24)
+      p = 255 * (x + y) / max
+    } bufferedImage.setRGB(x, y, p << 8)
+
+    // test IndexColorModel
+    /*val colorMapSize = width + height
+    val r, g, b = new Array[Byte](colorMapSize)
+    for {
+      i <- 0 until colorMapSize
+      c: Byte = (255 * i / colorMapSize).toByte
+    } { r(i) = c}
+    val icm = new IndexColorModel(8, colorMapSize, r, g, b)
+    bufferedImage = new BufferedImage(width, height, BufferedImage.TYPE_BYTE_INDEXED)
+    for {
+      x <- 0 until width
+      y <- 0 until height
+    } bufferedImage.getRaster.setSample(x, y, 0, x + y)*/
+
 
     override def paintComponent(g: Graphics2D) = {
       super.paintComponent(g)
