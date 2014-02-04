a317160723098f485ff8075be304d46b7d074e5d
Nemestóthy Zsombor <nemzsom@gmail.com>
2014-02-04 16:01:28 +0100

    Calculator refactor in progress

--- src/main/scala/hu/nemzsom/mandelbrot/Calculator.scala
+++ src/main/scala/hu/nemzsom/mandelbrot/Calculator.scala
@@ -4,11 +4,12 @@ import scala.annotation.tailrec
 import scala.concurrent._
 import scala.math._
 import rx.lang.scala.{Subscription, Observer, Observable}
+import java.util.concurrent.atomic.AtomicInteger
 
 /**
- * It calculate an area's points.
+ * It calculates an area's points.
  */
-class Calculator(mainArea: Area, plotter: Plotter)(implicit val ec: ExecutionContext) {
+class Calculator(mainArea: Area, plotter: Plotter)(implicit ec: ExecutionContext) {
 
   private object Config {
     val maxDividableSize = 17
@@ -17,11 +18,48 @@ class Calculator(mainArea: Area, plotter: Plotter)(implicit val ec: ExecutionCon
 
   import Config._
 
-  case class CalcStat(val all: Int, val settled: Int, val maxIter: Int)
+  case class CalcStat(total: Int, settled: Int, maxIter: Int)
 
-  private class IterationCycle(val maxIter: Int) {
+  /**
+   * Aggregates the various update processes result statistics in one iteration cycle.
+   * @param maxIter maximum iteration in this cycle
+   * @param observer the observer to notify at the end of this cycle
+   */
+  private class IterationCycle(val maxIter: Int, observer: Observer[CalcStat]) {
 
-    lazy val next: IterationCycle = new IterationCycle(maxIter + iterationStep)
+    /** Remaining points count in this cycle */
+    private val _remaining_total = new AtomicInteger(mainArea.size)
+    /** Settled points in this cycle */
+    private val _count_settled = new AtomicInteger
+    /** Already settled points at the beginning of this cycle. The previous cycle updates this when finished */
+    private var _initially_settled = 0
+
+    /** The next cycle */
+    lazy val next: IterationCycle = new IterationCycle(maxIter + iterationStep, observer)
+
+    /** It called when an update process finishes this cycle
+      * @param total total points updated by the process
+      * @param settled settled points count
+      */
+    def updated(total: Int, settled: Int): Unit = {
+      _count_settled.addAndGet(settled)
+      check(_remaining_total.addAndGet(-total))
+    }
+
+    private def previousFinished(initiallySettled: Int): Unit =
+      if (initiallySettled != 0) {
+        _initially_settled = initiallySettled
+        check(_remaining_total.addAndGet(-initiallySettled))
+      }
+
+    private def check(remaining: Int): Unit = {
+      assert(remaining >= 0, s"remaining: $remaining") // TODO remove after debug
+      if (remaining == 0) {
+        val settled = _count_settled.get
+        observer.onNext(CalcStat(mainArea.size - _initially_settled, settled, maxIter))
+        next.previousFinished(_initially_settled + settled)
+      }
+    }
   }
 
   private val subscription: Subscription = new Subscription {}
@@ -32,57 +70,77 @@ class Calculator(mainArea: Area, plotter: Plotter)(implicit val ec: ExecutionCon
     }
   )
 
-  private def calc(area: Area, cycle: IterationCycle): Unit = {
-    val (CalcStat(pointsCount, settled, _), _) = update(area.borders, cycle)
-    // uniform Unsettled borders
-    if (settled == 0) {
-      calc(area, cycle.next)
-    }
-    // uniform Outside borders
-    else if (pointsCount == settled && isUniform(area.borders)) {
-      val loc = area.topLeft.location
-      area.subArea(1,1, area.width - 2, area.height - 2).foreach { point =>
-        point.location = loc
-        plotter.plot(point)
+  private def calcWithBorder(area: Area, borderUpdater: Updater): Unit = {
+    val border = borderUpdater.points
+    borderUpdater.update().onSuccess { case (total, settled, nextUpdater) =>
+      if (isUniform(border)){
+        val loc = border.head.location
+        val cycle = borderUpdater.cycle
+        val areaSize = area.size
+        if (loc == Unsettled) { // Unsettled border
+          cycle.updated(areaSize, 0)
+          calcWithBorder(area, nextUpdater)
+        }
+        else {                                    // Inside or outside (with same iter) border
+          val middleArea = middle(area)
+          middleArea.foreach{ point =>
+            point.location = loc
+            plotter.plot(point)
+          }
+          cycle.updated(areaSize, areaSize)
+        }
+      }
+      else if (area.width > maxDividableSize || area.height > maxDividableSize) {
+
       }
     }
-    else if (area.width > maxDividableSize || area.height > maxDividableSize)
   }
 
   private def isUniform(points: Traversable[Point]): Boolean =
     points.forall(_.location == points.head.location)
 
-  private class Updater(points: Traversable[Point], cycle: IterationCycle) {
+  private def middle(area: Area) = area.subArea(1, 1, area.width - 2, area.height - 2)
 
-    private def update(): (CalcStat, Traversable[Point]) = {
-      var pointsCount = 0
+  private class Updater private(val points: Traversable[Point], val cycle: IterationCycle)(implicit ec: ExecutionContext) {
+
+    val maxIter = cycle.maxIter
+
+    def update(): Future[(Int, Int, Updater)] = future {
+      var total = 0
       var settled = 0
       var unsettledPoints = List.empty[Point]
       points.foreach { point =>
-        pointsCount += 1
-        iterate(point, cycle.maxIter)
-        if (point.location == Unsettled){
-          unsettledPoints = point :: unsettledPoints
-        }
-        else {
-          settled += 1
-          plotter.plot(point)
+        if (updatePoint(point)) {
+          total += 1
+          if (point.location == Unsettled) {
+            unsettledPoints = point :: unsettledPoints
+          }
+          else {
+            settled += 1
+            plotter.plot(point)
+          }
         }
       }
-      (CalcStat(pointsCount, settled, cycle.maxIter), unsettledPoints)
+      (total, settled, new Updater(unsettledPoints, cycle.next))
     }
 
     /**
-     * Performs the mandelbrot iteration on one point to the specified maxIteration.
+     * Updates one point to maxIter. The default implementation expects only [[hu.nemzsom.mandelbrot.Unsettled]] points
      * @param point to update
-     * @return true if the point escaped
+     * @return true if the point counts as updated in the current calculation
      */
-    protected def iterate(point: Point, maxIter: Int): Unit = if (point.iter < maxIter) {
-      val c = point.complexValue
-      if (point.iter == 0 && preCheck_isInside(c)){
-        point.location = Inside
-      }
-      else {
+    protected def updatePoint(point: Point): Boolean = {
+      iterate(point)
+      true
+    }
+
+    /**
+     * Performs the mandelbrot iteration on one point to the specified maxIteration. It expects only [[hu.nemzsom.mandelbrot.Unsettled]] points
+     * @param point to update
+     */
+    protected def iterate(point: Point): Unit =
+      if (point.iter < maxIter) {
+        val c = point.complexValue
         @tailrec def loop(iter: Int, z: Complex): Unit = {
           val escaped = z.escaped
           if (iter == maxIter || escaped) {
@@ -95,69 +153,61 @@ class Calculator(mainArea: Area, plotter: Plotter)(implicit val ec: ExecutionCon
         val z = point.iterValue
         loop(point.iter + 1, z * z + c)
       }
-    }
 
-    /** Optimization: Cardioid / bulb checking
-      * from http://en.wikipedia.org/wiki/Mandelbrot_set#Cardioid_.2F_bulb_checking
-      */
-    private def preCheck_isInside(c: Complex): Boolean = {
-      val q = pow(c.re - 0.25, 2) + pow(c.im, 2)
-      q * (q + (c.re - 0.25)) < pow(c.im, 2) / 4 ||
-        pow(c.re + 1, 2) + pow(c.im, 2) < 0.0625
-    }
 
     implicit class ComplexOps(c: Complex) {
 
       def escaped: Boolean = c.re * c.re + c.im * c.im > 2 * 2
     }
-  }
-
-  /*class Updater private(val area: Area, val points: Traversable[Point], val maxIter: Int)(implicit ec: ExecutionContext) {
-
-    lazy val update: Future[Updater] = future {
-      // debug BEGIN
-//      print(s"UPDATE $this")
-//      area.foreach( point => pixels(point.index) = 255 << 16)
-//      painter.repaint()
-//      debugQuene.take
-      // debug END
-      val unsettledPoints = updateAll()
-      // debug BEGIN
-//      area foreach color
-//      println(" DONE")
-      // debug END
-      //painter.repaint() // TODO repaint only after a whole update cycle
-      new Updater(area, unsettledPoints, maxIter + iterationStep)
-    }
-
-    /**
-     * Updates one point to maxIter. The default implementation expects only [[hu.nemzsom.mandelbrot.Unsettled]] points
-     * @param point to update
-     */
-    protected def updatePoint(point: Point): Unit = {
-      iterate(point)
-    }
-
-    override def toString: String =
-      s"Updater for $area, points size: ${points.size}, maxIter: $maxIter, id: ${hashCode}"
 
   }
 
   object Updater {
 
-    def apply(area: Area)(implicit ec: ExecutionContext): Updater = apply(area, iterationStep)
+    /**
+     * Creates a new updater, that can start the update process. The iterationCycle must be the first in the calculation
+     * @param points the points to update
+     * @param cycle the first iteration cycle
+     */
+    def start(points: Traversable[Point], cycle: IterationCycle)(implicit ec: ExecutionContext) = newUpdater(points, cycle, true)
 
-    def apply(area: Area, maxIter: Int)(implicit ec: ExecutionContext): Updater = new Updater(area, area, maxIter) {
+    /**
+     * Creates a new updater, that can continue the update process. The IterationCycle must be a continuation of a previous cycle
+     * @param points the points to update
+     * @param cycle the actual cycle
+     */
+    def continue(points: Traversable[Point], cycle: IterationCycle)(implicit ec: ExecutionContext) = newUpdater(points, cycle, false)
 
-    // It handles any Point
-    override def updatePoint(point: Point): Unit = {
-      if (point.location == Unsettled) {
-        if (point.iter == 0 && preCheck_isInside(point.complexValue)) point.location = Inside
-        else super.updatePoint(point)
+    /**
+     * Creates a new updater
+     * @param points the points to update
+     * @param cycle the actual cycle
+     * @param starter true, if this is the first IterationCycle in the calculation
+     */
+    private def newUpdater(points: Traversable[Point], cycle: IterationCycle, starter: Boolean)(implicit ec: ExecutionContext): Updater = new Updater(points, cycle) {
+
+      // It handles any Point
+      override def updatePoint(point: Point): Boolean = {
+        if (point.location == Unsettled) {
+          if (point.iter == 0 && preCheck_isInside(point.complexValue)) {
+            point.location = Inside
+            true
+          }
+          else super.updatePoint(point)
+        }
+        else starter
+      }
+
+      /** Optimization: Cardioid / bulb checking
+        * from http://en.wikipedia.org/wiki/Mandelbrot_set#Cardioid_.2F_bulb_checking
+        */
+      private def preCheck_isInside(c: Complex): Boolean = {
+        val q = pow(c.re - 0.25, 2) + pow(c.im, 2)
+        q * (q + (c.re - 0.25)) < pow(c.im, 2) / 4 ||
+          pow(c.re + 1, 2) + pow(c.im, 2) < 0.0625
       }
     }
   }
-}*/
 
 }
 
