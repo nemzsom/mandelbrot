f8a1a8f683418c6908824c50e9ec6c84960a80c8
Nemestóthy Zsombor <nemzsom@gmail.com>
2014-02-10 13:11:55 +0100

    updater trait and various implementations instead of anonymous classes

--- src/main/scala/hu/nemzsom/mandelbrot/Calculator.scala
+++ src/main/scala/hu/nemzsom/mandelbrot/Calculator.scala
@@ -184,83 +184,17 @@ class Calculator(mainArea: Area, plotter: Plotter)(implicit ec: ExecutionContext
   /** Inner area without borders */
   private def inner(area: Area) = area.subArea(1, 1, area.width - 2, area.height - 2)
 
-  class Updater private(val points: Traversable[Point], val cycle: IterationCycle)(implicit ec: ExecutionContext) {
+  trait Updater {
+
+    val hasPoints: Boolean
+    val points: Traversable[Point]
+    val cycle: IterationCycle
 
     lazy val maxIter = cycle.maxIter
 
-    def update(): Future[Updater] = future {
-      // debug BEGIN
-//      print(s"UPDATE at $cycle")
-//      points.foreach( point => debugPixels(point.index) = 255 << 16)
-//      debugPanel.repaint()
-//      debugQuene.take
-      // debug END
-      var total = 0
-      var settled = 0
-      var unsettledPoints = List.empty[Point]
-      points.foreach { point =>
-        if (updatePoint(point)) {
-          total += 1
-          if (point.location == Unsettled) {
-            unsettledPoints = point :: unsettledPoints
-          }
-          else {
-            settled += 1
-            plotter.plot(point)
-          }
-        }
-      }
-      // debug BEGIN
-//      points foreach plotter.plot
-//      logger.trace(s" DONE. total: $total, settled: $settled")
-//      debugPanel.repaint()
-      // debug END
-      cycle.updated(total, settled)
-      if (total == settled) Updater.empty(cycle.next)
-      else new Updater(unsettledPoints, cycle.next)
-    }
-
-    val hasPoints: Boolean = true
-
-    /**
-     * Updates one point to maxIter. The default implementation expects only [[hu.nemzsom.mandelbrot.Unsettled]] points
-     * @param point to update
-     * @return true if the point counts as updated in the current calculation
-     */
-    protected def updatePoint(point: Point): Boolean = {
-      iterate(point)
-      true
-    }
-
-    /**
-     * Performs the mandelbrot iteration on one point to the specified maxIteration. It expects only [[hu.nemzsom.mandelbrot.Unsettled]] points
-     * @param point to update
-     */
-    protected def iterate(point: Point): Unit =
-      if (point.iter < maxIter) {
-        val c = point.complexValue
-        @tailrec def loop(iter: Int, z: Complex): Unit = {
-          val escaped = z.escaped
-          if (iter == maxIter || escaped) {
-            point.iter = iter
-            point.iterValue = z
-            if (escaped) point.location = Outside(iter)
-          }
-          else loop(iter + 1, z * z + c)
-        }
-        val z = point.iterValue
-        loop(point.iter + 1, z * z + c)
-      }
-
-
-    implicit class ComplexOps(c: Complex) {
-
-      def escaped: Boolean = c.re * c.re + c.im * c.im > 2 * 2
-    }
-
+    def update(): Future[Updater]
   }
 
-  // TODO define a trait for updater and provide various implementations instead of subclasses
   object Updater {
 
     /**
@@ -268,36 +202,98 @@ class Calculator(mainArea: Area, plotter: Plotter)(implicit ec: ExecutionContext
      * @param points the points to update. This update must be the first for them in the actual calculation
      * @param cycle the actual iteration cycle
      */
-    def start(points: Traversable[Point], cycle: IterationCycle) = newUpdater(points, cycle, starter = true)
+    def start(points: Traversable[Point], cycle: IterationCycle) = new StartUpdater(points, cycle, firstStart = true)
 
     /**
      * Creates a new updater, that can continue the update process for the provided points
      * @param points the points to update. They must have updated at least once in the actual calculation
      * @param cycle the actual iteration cycle
      */
-    def continue(points: Traversable[Point], cycle: IterationCycle) = newUpdater(points, cycle, starter = false)
+    def continue(points: Traversable[Point], cycle: IterationCycle) = new StartUpdater(points, cycle, firstStart = false)
 
     def merge(updaterA: Updater, updaterB: Updater) = {
-      require(updaterA.cycle == updaterB.cycle, s"updaterA cycle: ${updaterA.cycle}, updaterB cycle: ${updaterB.cycle}")
+      //require(updaterA.cycle == updaterB.cycle, s"updaterA cycle: ${updaterA.cycle}, updaterB cycle: ${updaterB.cycle}")
       if (updaterA.hasPoints || updaterB.hasPoints) continue(updaterA.points ++ updaterB.points, updaterA.cycle)
       else empty(updaterA.cycle)
     }
 
-    def empty(cycle: IterationCycle) = new Updater(Traversable(), cycle) {
-      override val hasPoints = false
+    def empty(cycle: IterationCycle) = new EmptyUpdater(cycle)
+
+    def completed(points: Traversable[Point], cycle: IterationCycle) = new CompletedUpdater(points, cycle)
+
+    class CalcUpdater(val points: Traversable[Point], val cycle: IterationCycle) extends Updater {
+
+      val hasPoints = true
+
+      def update(): Future[Updater] = future {
+        // debug BEGIN
+        //      print(s"UPDATE at $cycle")
+        //      points.foreach( point => debugPixels(point.index) = 255 << 16)
+        //      debugPanel.repaint()
+        //      debugQuene.take
+        // debug END
+        var total = 0
+        var settled = 0
+        var unsettledPoints = List.empty[Point]
+        points.foreach { point =>
+          if (updatePoint(point)) {
+            total += 1
+            if (point.location == Unsettled) {
+              unsettledPoints = point :: unsettledPoints
+            }
+            else {
+              settled += 1
+              plotter.plot(point)
+            }
+          }
+        }
+        // debug BEGIN
+        //      points foreach plotter.plot
+        //      logger.trace(s" DONE. total: $total, settled: $settled")
+        //      debugPanel.repaint()
+        // debug END
+        cycle.updated(total, settled)
+        if (total == settled) empty(cycle.next)
+        else new CalcUpdater(unsettledPoints, cycle.next)
+      }
+
+      /**
+       * Updates one point to maxIter. The default implementation expects only [[hu.nemzsom.mandelbrot.Unsettled]] points
+       * @param point to update
+       * @return true if the point counts as updated in the current calculation
+       */
+      protected def updatePoint(point: Point): Boolean = {
+        iterate(point)
+        true
+      }
+
+      /**
+       * Performs the mandelbrot iteration on one point to the specified maxIteration. It expects only [[hu.nemzsom.mandelbrot.Unsettled]] points
+       * @param point to update
+       */
+      protected def iterate(point: Point): Unit =
+        if (point.iter < maxIter) {
+          val c = point.complexValue
+          @tailrec def loop(iter: Int, z: Complex): Unit = {
+            val escaped = z.escaped
+            if (iter == maxIter || escaped) {
+              point.iter = iter
+              point.iterValue = z
+              if (escaped) point.location = Outside(iter)
+            }
+            else loop(iter + 1, z * z + c)
+          }
+          val z = point.iterValue
+          loop(point.iter + 1, z * z + c)
+        }
+
+      implicit class ComplexOps(c: Complex) {
+
+        def escaped: Boolean = c.re * c.re + c.im * c.im > 2 * 2
+      }
     }
 
-    def completed(points: Traversable[Point], cycle: IterationCycle) = new Updater(points, cycle) {
-      override def update() = Future.successful(continue(points, cycle.next))
-  }
-
-    /**
-     * Creates a new updater
-     * @param points the points to update
-     * @param cycle the actual iteration cycle
-     * @param starter true, if this updater starts the calculation for the points
-     */
-    private def newUpdater(points: Traversable[Point], cycle: IterationCycle, starter: Boolean)(implicit ec: ExecutionContext): Updater = new Updater(points, cycle) {
+    class StartUpdater(points: Traversable[Point], cycle: IterationCycle, firstStart: Boolean) extends CalcUpdater(points, cycle) {
 
       // It handles any Point
       override def updatePoint(point: Point): Boolean = {
@@ -308,7 +304,7 @@ class Calculator(mainArea: Area, plotter: Plotter)(implicit ec: ExecutionContext
           }
           else super.updatePoint(point)
         }
-        else starter
+        else firstStart
       }
 
       /** Optimization: Cardioid / bulb checking
@@ -319,6 +315,21 @@ class Calculator(mainArea: Area, plotter: Plotter)(implicit ec: ExecutionContext
         q * (q + (c.re - 0.25)) < pow(c.im, 2) / 4 ||
           pow(c.re + 1, 2) + pow(c.im, 2) < 0.0625
       }
+    }
+
+    class EmptyUpdater(val cycle: IterationCycle) extends Updater {
+
+      val hasPoints = false
+      val points = Traversable()
+
+      override def update(): Nothing = throw new RuntimeException("update on empty updater") // TODO proper exception
+    }
+
+    class CompletedUpdater(val points: Traversable[Point], val cycle: IterationCycle) extends Updater {
+
+      val hasPoints = true
+
+      override def update() = Future.successful(continue(points, cycle.next))
     }
   }
 
